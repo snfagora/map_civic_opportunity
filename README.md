48684ee29306f2c41f01a1d47b1406868e980bf7
Jae Yeon Kim <jaeyeonkim@codeforamerica.org>
2023-03-02 07:26:10 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# map_civic_opportunity
+# Mapping the Civic Opportunity in the US 
+
+Authors: de Vries, Kim, and Han 
+Last update: [TBD]
+
+## Environments 
+
+## Data 
+
+### Raw data 
+
+### Processed data 
+
+## Code 
+
+- Tables 
+
+- Figures 
---
+++ functions/utils.R
@@ -0,0 +1,40 @@
+custom_theme <- function() {
+  
+  theme_bw(base_size = 13) +
+    theme(
+      aspect.ratio = 1.2,
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank(),
+      panel.grid.major.y = element_blank(),
+      panel.grid.minor.y = element_blank(),
+      axis.text.x = element_text(margin = margin(t = 6)),
+      plot.title = element_text(size = 12),
+      plot.subtitle = element_text(size = 10),
+      plot.caption = element_text(colour = "grey50", hjust = 0)
+    )
+  
+}
+
+cut_pop <- function(n_top) {
+  
+  top_fips <- dd %>%
+    ungroup() %>%
+    group_by(urban_suburban_rural) %>%
+    slice_max(order_by = TotalPopEst2019, prop = n_top) %>%
+    pull(FIPS)
+  
+  dd <- dd %>%
+    mutate(top = ifelse(FIPS %in% top_fips, 1, 0))
+  
+  out <- dd %>%
+    filter(top == 1) %>%
+    ungroup() %>%
+    group_by(urban_suburban_rural) %>% 
+    #top50) %>%
+    do(tidy(standardize(lm_robust(formula = opc_tier ~ race_per_white_nonhispanic + per_poverty + college_educ, data = ., weights = TotalPopEst2019)))) %>% 
+    filter(!str_detect(term, "(Intercept)")) %>%
+    mutate(top_n = n_top)
+  
+  return(out)
+  
+}
