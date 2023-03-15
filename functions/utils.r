8d26cbcb421076c1fb6d0416671159a50a219e00
Jae Yeon Kim <jaeykim2@gmail.com>
2023-03-14 21:33:31 -0700

    update figure1

--- functions/utils.r
+++ functions/utils.r
@@ -1,5 +1,4 @@
-custom_theme <- function() {
-  
+$)Ccustom_theme <- function() {
   theme_bw(base_size = 13) +
     theme(
       aspect.ratio = 1.2,
@@ -11,31 +10,28 @@ custom_theme <- function() {
       plot.title = element_text(size = 12),
       plot.subtitle = element_text(size = 10),
       plot.caption = element_text(colour = "grey50", hjust = 0),
-      legend.position = "right"
+      legend.position = "bottom"
     )
-  
 }
 
 cut_pop <- function(n_top) {
-  
   top_fips <- dd %>%
     ungroup() %>%
     group_by(urban_suburban_rural) %>%
     slice_max(order_by = TotalPopEst2019, prop = n_top) %>%
     pull(FIPS)
-  
+
   dd <- dd %>%
     mutate(top = ifelse(FIPS %in% top_fips, 1, 0))
-  
+
   out <- dd %>%
     filter(top == 1) %>%
     ungroup() %>%
-    group_by(urban_suburban_rural) %>% 
-    #top50) %>%
-    do(tidy(standardize(lm_robust(formula = opc_tier ~ race_per_white_nonhispanic + per_poverty + college_educ + foreign_born_pct, data = ., weights = TotalPopEst2019)))) %>% 
+    group_by(urban_suburban_rural) %>%
+    # top50) %>%
+    do(tidy(standardize(lm_robust(formula = opc_tier ~ race_per_white_nonhispanic + per_poverty + college_educ, data = ., weights = TotalPopEst2019)))) %>%
     filter(!str_detect(term, "(Intercept)")) %>%
     mutate(top_n = n_top)
-  
+
   return(out)
-  
 }
