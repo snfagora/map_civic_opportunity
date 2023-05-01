6a99f7ebc942ab196ced4be4d47d3bb7bfcab070
Jae Yeon Kim <jaeyeonkim@codeforamerica.org>
2023-04-30 17:46:48 -0700

    the code that produces regregssion tables

--- src/regression_tables.R
+++ src/regression_tables.R
@@ -1,10 +1,48 @@
 
 if (!require(pacman)) install.packages("pacman")
 
-pacman::p_load(dplyr, here, clipr, ggplot2, readr, gt, modelsummary)
+pacman::p_load(dplyr, here, clipr, ggplot2, readr, gt, modelsummary, stringr, effectsize, datawizard, purrr)
 
 ggplot2::theme_set(theme_minimal())
 
-mods <- read_rds(here("processed_data", "mutual_aid_models.rds"))
+desc_files <- list.files(here("processed_data"))[stringr::str_detect(list.files(here("processed_data")), "txt")]
 
-modelsummary(mods)
+desc_files
+
+rds_files <- list.files(here("processed_data"))[stringr::str_detect(list.files(here("processed_data")), "rds")]
+
+# Mutual aid model 
+
+mad <- read_rds(here("processed_data", "mutual_aid_models.rds"))
+
+mad_models <- list(
+   "Civic Opportunity (base)" = mad$opp_orgs_min,
+   "Civic Opportunity (full)" = mad$opp_orgs_full,
+   "Organizational density (base)" = mad$all_orgs_min,
+   "Organizational density (full)" = mad$all_orgs_full,
+   "Social capital (full)" = mad$soc_cap_full
+)
+
+mad_cm <- c(
+  "opc" = "Civic opportunity",
+  "apc" = "Organizational density",
+  "socialcap" = "Social capital",
+  "gop.tier" = "GOP presidential vote (%)",
+  "Age65AndOlderPct2010" = "Age 65 or older (%)",
+  "AsianNonHispanicPct2010" = "Asian (%)",
+  "BlackNonHispanicPct2010" = "Black (%)",
+  "HispanicPct2010" = "Hispanic (%)",
+  "NativeAmericanNonHispanicPct2010" = "Native American (%)",
+  "RUCC" = "Rural/Urban continuity"
+)
+
+modelsummary(mad_models, 
+             fmt = 3,
+             vcov = "robust",
+             estimate = c("{estimate} ({std.error}){stars}"),
+             statistic = NULL,
+             coef_omit = "Intercept",
+             coef_map = mad_cm,
+             output = here("outputs", "reg_base.docx"))
+  
+# Vaccine model 
